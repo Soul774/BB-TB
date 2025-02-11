59e24c6e9da83f7a800a9de71c2ef8b6e08017e6
Soul <76665784+Soul774@users.noreply.github.com>
2025-02-10 22:13:07 -0500

    Update TB.py

--- TB.py
+++ TB.py
@@ -10,8 +10,8 @@ gdi32 = ctypes.windll.gdi32
 hdc = user32.GetDC(0) # get device context
 
 tolerance = 120 # raise if it doesnt dtc
-scan_range = 50 # raise if miss in shiftlock
-target_color = (0, 255, 0) # green #00FF00v
+scanRange = 50 # raise if miss in shiftlock
+targetColor = (0, 255, 0) # green #00FF00v
 
 toggle = False
 
@@ -41,8 +41,8 @@ while True:
     if toggle:
         x, y = mouse.get_position()
         green_detected = any(
-            isColorClose(pixelColor(x + dx, y + dy), target_color, tolerance)
-            for dx, dy in [(-scan_range, 0), (scan_range, 0), (0, -scan_range), (0, scan_range)]
+            isColorClose(pixelColor(x + dx, y + dy), targetColor, tolerance)
+            for dx, dy in [(-scanRange, 0), (scanRange, 0), (0, -scanRange), (0, scanRange)]
         )
         if green_detected:
             mouse.click('left')
