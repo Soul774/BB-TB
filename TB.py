f505e667f7305da952df929ab975b5469a8a0931
Soul <76665784+Soul774@users.noreply.github.com>
2025-02-10 22:06:53 -0500

    Update TB.py
    faster
    fixed over parrying ( spamming ) 
    replaced screenshotting with windows api pixel reading (fixes fps drops and delays)

--- TB.py
+++ TB.py
@@ -1,17 +1,29 @@
-import pyautogui
+import ctypes
 import keyboard
-import numpy as np
-import cv2
+import mouse
+import time
 
-tolerance = 90
-scan_range = 30
-target_color = (0, 255, 0) # green #00FF00
+# windows api functions
+user32 = ctypes.windll.user32
+gdi32 = ctypes.windll.gdi32
+
+hdc = user32.GetDC(0) # get device context
+
+tolerance = 120 # raise if it doesnt dtc
+scan_range = 50 # raise if miss in shiftlock
+target_color = (0, 255, 0) # green #00FF00v
 
 toggle = False
 
-def isColor(pixel, target_color, tolerance):
-    diff = np.abs(np.array(pixel) - np.array(target_color))
-    return np.all(diff <= tolerance)
+def pixelColor(x, y):
+    pixel = gdi32.GetPixel(hdc, x, y)
+    r = pixel & 0xFF
+    g = (pixel >> 8) & 0xFF
+    b = (pixel >> 16) & 0xFF
+    return (r, g, b)
+
+def isColorClose(color1, color2, tolerance):
+    return all(abs(c1 - c2) <= tolerance for c1, c2 in zip(color1, color2))
 
 def toggleTB():
     global toggle
@@ -20,30 +32,19 @@ def toggleTB():
 
 keyboard.add_hotkey('v', toggleTB)
 
-print("Press 'V' to toggle, 'Esc' to exit.")
+print("TB: Press 'V' to toggle, 'Esc' to exit.")
 
 while True:
-    if toggle:
-        x, y = pyautogui.position()  # cursor pos
-        region = (x - scan_range, y - scan_range, scan_range * 2 + 1, scan_range * 2 + 1) # region defining
-
-        screenshot = pyautogui.screenshot(region=region)
-        screenshot = np.array(screenshot)
-        screenshot = cv2.cvtColor(screenshot, cv2.COLOR_RGB2BGR)
-
-        foundColor = False
-        for dx in range(screenshot.shape[0]):
-            for dy in range(screenshot.shape[1]):
-                pixel_color = screenshot[dx, dy]
-                if isColor(pixel_color, target_color, tolerance):
-                    foundColor = True
-                    break
-            if foundColor:
-                break
-        if foundColor:
-            pyautogui.click()
-            print("Parried.")
-
-    if keyboard.is_pressed('esc'):
-        print("Exiting..")
+    if keyboard.is_pressed('esc'): # kill script
         break
+
+    if toggle:
+        x, y = mouse.get_position()
+        green_detected = any(
+            isColorClose(pixelColor(x + dx, y + dy), target_color, tolerance)
+            for dx, dy in [(-scan_range, 0), (scan_range, 0), (0, -scan_range), (0, scan_range)]
+        )
+        if green_detected:
+            mouse.click('left')
+            time.sleep(0.1)
+            print("Parried.")
